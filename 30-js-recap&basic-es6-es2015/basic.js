90f702117206862cccea05850b0c56b884a79cad
mehesultana <sultanamehe01@gmail.com>
2021-08-23 00:06:01 +0600

    basic javascript

---
+++ 30-js-recap&basic-es6-es2015/basic.js
@@ -0,0 +1,40 @@
+//30-1 Simple Recap of JavaScript six core parts
+// variable
+var deposit = 400;
+
+// condition
+if (deposit > 500) {
+} else if (deposit < 200) {
+} else if (deposit == 500) {
+} else if (deposit != 500) {
+} else if (deposit >= 500) {
+} else if (deposit <= 500) {
+} else {
+}
+
+// array
+const numbers = [45, 6587, 124, 45, 1, 365];
+const numberCount = numbers.length;
+numbers.push(111);
+numbers.pop();
+numbers[2] = 555;
+
+// check whether 222 included in the array
+if (numbers.indexOf(222) != -1) {
+}
+if (numbers.includes(222)) {
+}
+
+//loop
+//while, for
+for (const number of numbers) {
+}
+
+// function
+function fullName(first, second) {
+    const name = first + ' ' + second;
+}
+const person = fullName('Rahim', 'Ali');
+
+// object
+const bottle = { color: 'yellow', contain: 'water', price: 50 };
