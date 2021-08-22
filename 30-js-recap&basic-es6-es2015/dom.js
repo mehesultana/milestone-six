aa7be74e27c66a7ad0933963d730576565b96808
mehesultana <sultanamehe01@gmail.com>
2021-08-23 01:13:36 +0600

    dom

---
+++ 30-js-recap&basic-es6-es2015/dom.js
@@ -0,0 +1,25 @@
+//30-2 Recap of DOM Manipulation and event handler
+
+document.getElementById('add-border').addEventListener('click', function () {
+    //console.log('add border click');
+    const container = document.getElementById('friend-container');
+    container.style.border = '3px solid yellow';
+});
+
+function addBackgroundColor() {
+    const friends = document.getElementsByClassName('friend');
+    for (const friend of friends) {
+        friend.style.backgroundColor = 'lightblue';
+    }
+}
+
+document.getElementById('add-friend').addEventListener('click', function () {
+    const container = document.getElementById('friend-container');
+    const friendDiv = document.createElement('div');
+    friendDiv.classList.add('friend');
+    friendDiv.innerHTML = `
+            <h3 class="friend-name">New Friend</h3>
+            <p>Quam, sapiente.</p>
+    `;
+    container.appendChild(friendDiv);
+});
