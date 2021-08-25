6c4f58d585090df4cf4fce64c73e707fc03a75f2
mehesultana <sultanamehe01@gmail.com>
2021-08-25 23:51:36 +0600

    API

---
+++ 32-getting-started-with-api/js/json.js
@@ -0,0 +1,8 @@
+//32-1 Introduction And What Is An Api
+//32-2 Intro To Json, Json Structure, Parse, Stringify
+// JavaScript Object Notation
+// JSON
+const user = { id: 11, name: 'Gorib Amir', job: 'actor' };
+const stringified = JSON.stringify(user);
+// console.log(user);
+// console.log(stringified);
