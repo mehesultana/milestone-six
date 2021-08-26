f79bf1b46fc6ebd6674971506740e4f2bbbc3a6a
mehesultana <sultanamehe01@gmail.com>
2021-08-27 02:37:49 +0600

    display countries name

--- 33-api-examples&edge-cases/js/countries.js
+++ 33-api-examples&edge-cases/js/countries.js
@@ -22,3 +22,19 @@ const displayCountries = (counties) => {
         countriesDiv.appendChild(div);
     });
 };
+const loadCountryByName = (name) => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => displayCountryDetail(data[0]));
+};
+
+const displayCountryDetail = (country) => {
+    console.log(country);
+    const countryDiv = document.getElementById('country-detail');
+    countryDiv.innerHTML = `
+        <h5>${country.name}</h4>
+        <p>population: ${country.population}</p>
+        <img width="200px" src="${country.flag}">
+    `;
+};
