d377c21a1320ef4eea7963db69f7c3ac42317174
mehesultana <sultanamehe01@gmail.com>
2021-08-26 21:08:00 +0600

    display post

--- 32-getting-started-with-api/js/posts.js
+++ 32-getting-started-with-api/js/posts.js
@@ -19,3 +19,35 @@ function displayPosts(posts) {
         console.log(post);
     }
 }
+
+function addAPost2() {
+    fetch('https://jsonplaceholder.typicode.com/pusts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'My new post',
+            body: 'This is my posts',
+            userId: 1,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+}
+
+function addAPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'My new post',
+            body: 'This is my posts',
+            userId: 1,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+}
