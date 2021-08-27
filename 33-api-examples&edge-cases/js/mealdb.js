d20cfd8a6287db6e6c2766c214ea1062a74eb024
mehesultana <sultanamehe01@gmail.com>
2021-08-27 20:51:13 +0600

    handle fetch error

--- 33-api-examples&edge-cases/js/mealdb.js
+++ 33-api-examples&edge-cases/js/mealdb.js
@@ -1,21 +1,26 @@
 //33-5 Explore meal db api and create dynamic url to load meals
-
+document.getElementById('error-message').style.display = 'none';
 const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     //console.log(searchText);
     //clear data
     searchField.value = '';
+    document.getElementById('error-message').style.display = 'none';
     if (searchText == '') {
         // please write something to display
     } else {
-        //load data
+        // load data
         const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-        //console.log(url);
         fetch(url)
             .then((res) => res.json())
-            .then((data) => displaySearchReasult(data.meals));
+            .then((data) => displaySearchResult(data.meals))
+            .catch((error) => displayError(error));
     }
+};
+
+const displayError = (error) => {
+    document.getElementById('error-message').style.display = 'block';
 };
 
 //33-6 Display dynamic search result using bootstrap cards
