5365098a6e74c9872cdff9ef0ba5afa306bf7dd0
mehesultana <sultanamehe01@gmail.com>
2021-08-27 18:05:53 +0600

    Create dynamic url

--- 33-api-examples&edge-cases/js/mealdb.js
+++ 33-api-examples&edge-cases/js/mealdb.js
@@ -34,3 +34,29 @@ const displaySearchReasult = (meals) => {
         searchResult.appendChild(div);
     });
 };
+
+//33-7 Create dynamic url based on click and display data
+
+const loadMealDetail = (mealId) => {
+    console.log(mealId);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => displayMealDetail(data.meals[0]));
+};
+
+const displayMealDetail = (meal) => {
+    console.log(meal);
+    const mealDetails = document.getElementById('meal-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+        <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+    </div>
+    `;
+    mealDetails.appendChild(div);
+};
