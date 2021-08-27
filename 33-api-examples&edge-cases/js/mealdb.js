6acc7bc4966dcb99d75d72b7430583e661413b5d
mehesultana <sultanamehe01@gmail.com>
2021-08-27 17:38:39 +0600

    Explore meal db

--- 33-api-examples&edge-cases/js/mealdb.js
+++ 33-api-examples&edge-cases/js/mealdb.js
@@ -1,4 +1,5 @@
 //33-5 Explore meal db api and create dynamic url to load meals
+
 const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
@@ -10,5 +11,26 @@ const searchFood = () => {
     //console.log(url);
     fetch(url)
         .then((res) => res.json())
-        .then((data) => console.log(data));
+        .then((data) => displaySearchReasult(data.meals));
+};
+
+//33-6 Display dynamic search result using bootstrap cards
+const displaySearchReasult = (meals) => {
+    //console.log(meals);
+    const searchResult = document.getElementById('search-result');
+    meals.forEach((meal) => {
+        // console.log(meal);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card h-100">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    });
 };
