eef0d12543276cab926742180d72f4acb87fd27b
mehesultana <sultanamehe01@gmail.com>
2021-08-30 00:57:54 +0600

    data types

---
+++ 34-javascript-tricky-concepts/primitive.js
@@ -0,0 +1,28 @@
+//34-2 Primitive data type and non-primitive data type
+/* 
+Data types
+
+primitive data types:
+1. number
+2. string
+3. boolean
+4. undefined
+5. null
+6. symbol
+
+non-primitive:
+7. object
+*/
+
+let a = 'hello';
+let b = a;
+// console.log(a, b);
+a = 'gello';
+// console.log(a, b);
+
+const x = { job: 'web developer' };
+const y = x;
+console.log(x, y);
+// x.job = 'front end developer';
+y.job = 'front end developer';
+console.log(x, y);
