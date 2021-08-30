1f52ba9592ffc43f1a70ceefc4a833ee3275189d
mehesultana <sultanamehe01@gmail.com>
2021-08-31 03:11:23 +0600

    this keyword

---
+++ 35-advanced-javascript-object-concepts/this.js
@@ -0,0 +1,27 @@
+//35-8 ( super advanced) Understand this keyword in JavaScript
+//console.log(this);
+const kibria = {
+    id: 101,
+    money: 5000,
+    name: 'RJ Kibria',
+    treatDeyArrow: () => {
+        console.log(this);
+    },
+    treatDeyInside: function () {
+        const myArrow = () => console.log(this);
+        myArrow();
+    },
+    treatDey: function (expense) {
+        this.money = this.money - expense;
+        console.log('here', this);
+        return this.money;
+    },
+};
+const heroBalam = {
+    id: 102,
+    money: 6000,
+    name: 'Hero Balam',
+};
+function add() {
+    console.log(this);
+}
