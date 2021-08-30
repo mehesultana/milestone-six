9a02ae5ab2af72cbe09b897b8deac06e7481807c
mehesultana <sultanamehe01@gmail.com>
2021-08-30 23:36:42 +0600

    different way to Create Object

---
+++ 35-advanced-javascript-object-concepts/objectCreate.js
@@ -0,0 +1,27 @@
+// 1. using object literal
+const student = { name: 'Sakib AL Hasaan', job: 'cricketer' };
+// 2. constructor
+const person = new Object();
+console.log(person);
+
+// 3.
+//const human = Object.create(null);
+const human = Object.create(student);
+console.log(human.job);
+
+//4. class
+class People {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const people1 = new People('Manush', 12);
+console.log(people1);
+
+// 5. function
+function Manush(name) {
+    this.name = name;
+}
+const man = new Manush('kader');
+console.log(man);
