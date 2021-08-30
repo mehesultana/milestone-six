2988e15fe42604140b0649a15d3d7c7b65706f2b
mehesultana <sultanamehe01@gmail.com>
2021-08-30 20:30:35 +0600

    module summary

---
+++ 34-javascript-tricky-concepts/summary.js
@@ -0,0 +1,13 @@
+//34-9 Module summary pass by reference pass by value
+const a = 'Alim uddin';
+const b = 23;
+const c = true;
+// null
+// undefined
+
+// primitive
+// object
+
+const nums = [23, 12, 56, 34];
+// console.log(typeof nums);
+// Array.isArray(nums)
