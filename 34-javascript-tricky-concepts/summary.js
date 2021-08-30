162aae55c51d974dff0f98e033c91b382e00bc7d
mehesultana <sultanamehe01@gmail.com>
2021-08-30 20:36:34 +0600

    module summary

--- 34-javascript-tricky-concepts/summary.js
+++ 34-javascript-tricky-concepts/summary.js
@@ -11,3 +11,20 @@ const c = true;
 const nums = [23, 12, 56, 34];
 // console.log(typeof nums);
 // Array.isArray(nums)
+
+function triple(x, y, z) {
+    x.age = 88888;
+    // y = 2222;
+    // z = 333;
+}
+// console.log(typeof triple);
+// console.log(triple.length);
+const num1 = 3;
+const num2 = 5;
+const num3 = 7;
+// triple(num1, num2, num3);
+const myObj = { name: 'kuddus', age: 17 };
+triple(myObj);
+console.log(myObj);
+// console.log(num1, num2, num3);
+// console.log(typeof null);
