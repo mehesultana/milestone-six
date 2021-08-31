b515e0e276f15788a21c463c002e5c36a76815f3
mehesultana <sultanamehe01@gmail.com>
2021-08-31 16:59:41 +0600

    dynamic url to load teams

--- 33.5-practice-problems-sports-db/sports.js
+++ 33.5-practice-problems-sports-db/sports.js
@@ -1,5 +1,15 @@
 const searchTeam = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    console.log(searchText);
+    //console.log(searchText);
+
+    searchField.value = '';
+
+    const url = `https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${searchText}`;
+
+    //console.log(url);
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => console.log(data.teams));
 };
