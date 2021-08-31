fcccd5cd06f7d6ed0cc0a5b56740f8c2b6034b79
mehesultana <sultanamehe01@gmail.com>
2021-09-01 02:42:59 +0600

    problem 3.2 solved

--- 35.5-have-a-world-tour-with-api/es6/practice.js
+++ 35.5-have-a-world-tour-with-api/es6/practice.js
@@ -37,3 +37,18 @@ const favouritePlace = ['kajir bazar', 'audi'];
 const fullInfo = `This is ${user.info.name}. My friend is ${friends[1]} . our favourite place is ${favouritePlace[1]}`;
 
 console.log(fullInfo);
+
+// expected output: Array [8, 6, 7, 9]
+
+//(3.1) no parameter arrow function . return 89
+
+//(3.2)single parameter arrow function . will divide input by 7
+
+const doMath = (x) => {
+    const divide = x / 7;
+    return divide;
+};
+const result = doMath(490);
+console.log(result);
+
+//(3.3)
