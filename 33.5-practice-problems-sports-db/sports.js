eae7376a59cb6ba3b7e8c047836416c980e443a7
mehesultana <sultanamehe01@gmail.com>
2021-08-31 18:00:33 +0600

    display dynamic search result

--- 33.5-practice-problems-sports-db/sports.js
+++ 33.5-practice-problems-sports-db/sports.js
@@ -23,10 +23,10 @@ const displaySearchResult = (teams) => {
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-          <img src="..." class="card-img-top" alt="..." />
-             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <img src="${team.strTeamBadge}" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                    <h5 class="card-name mt-3">${team.strTeam}</h5>
+                    <p class="card-text">${team.strDescriptionEN.slice(0, 200)}</p>
                     </div>
         </div>
         `;
