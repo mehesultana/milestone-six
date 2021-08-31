db6a887902ba696dac6a6178dcbe4992b4820939
mehesultana <sultanamehe01@gmail.com>
2021-08-31 17:21:20 +0600

    display dynamic search result

--- 33.5-practice-problems-sports-db/sports.js
+++ 33.5-practice-problems-sports-db/sports.js
@@ -11,5 +11,25 @@ const searchTeam = () => {
 
     fetch(url)
         .then((res) => res.json())
-        .then((data) => console.log(data.teams));
+        .then((data) => displaySearchResult(data.teams));
+};
+
+const displaySearchResult = (teams) => {
+    //console.log(teams);
+    const searchResult = document.getElementById('search-result');
+    teams.forEach((team) => {
+        console.log(team);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+          <img src="..." class="card-img-top" alt="..." />
+             <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    });
 };
