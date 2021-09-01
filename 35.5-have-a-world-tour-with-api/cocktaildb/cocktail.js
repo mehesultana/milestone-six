933686f7253604f0c7f2473698ea93ea7b330c0f
mehesultana <sultanamehe01@gmail.com>
2021-09-01 19:30:26 +0600

    display data

--- 35.5-have-a-world-tour-with-api/cocktaildb/cocktail.js
+++ 35.5-have-a-world-tour-with-api/cocktaildb/cocktail.js
@@ -17,11 +17,11 @@ const displaySearchResult = (drinks) => {
     //console.log(drinks);
     const searchResult = document.getElementById('search-result');
     drinks.forEach((drink) => {
-        console.log(drink);
+        //console.log(drink);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card">
+        <div onclick="loadDrinkDetail(${drink.idDrink})" class="card h-100">
                     <img src="${drink.strDrinkThumb}" class="card-img-top" alt="..." />
                     <div class="card-body">
                         <h5 class="card-title">${drink.strDrink}</h5>
@@ -31,4 +31,29 @@ const displaySearchResult = (drinks) => {
         `;
         searchResult.appendChild(div);
     });
+};
+
+const loadDrinkDetail = (drinkId) => {
+    //console.log(drinkId);
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`;
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => displayDrinkDetails(data.drinks[0]));
+};
+
+const displayDrinkDetails = (drink) => {
+    console.log(drink);
+    const drinkDetails = document.getElementById('drink-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${drink.strDrinkThumb}" class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">${drink.strDrink}</h5>
+                    <p class="card-text">${drink.strInstructions.slice(0, 150)}</p>
+                    <a href="${drink.strVideo}" class="btn btn-warning text-dark bg-opacity-10">See Instructions</a>
+                </div>
+    `;
+    drinkDetails.appendChild(div);
 };
