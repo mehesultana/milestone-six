13e8ffc2be73f24ed2f748d2b975b0ef4ad01568
mehesultana <sultanamehe01@gmail.com>
2021-09-01 18:50:27 +0600

    Display dynamic search result

--- 35.5-have-a-world-tour-with-api/cocktaildb/cocktail.js
+++ 35.5-have-a-world-tour-with-api/cocktaildb/cocktail.js
@@ -1,6 +1,6 @@
 //API-(Search cocktail by name) https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
 
-const searchCocktail = () => {
+const searchDrink = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     //console.log(searchText);
@@ -10,5 +10,25 @@ const searchCocktail = () => {
     //console.log(url);
     fetch(url)
         .then((res) => res.json())
-        .then((data) => console.log(data.drinks));
+        .then((data) => displaySearchResult(data.drinks));
+};
+
+const displaySearchResult = (drinks) => {
+    //console.log(drinks);
+    const searchResult = document.getElementById('search-result');
+    drinks.forEach((drink) => {
+        console.log(drink);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+                    <img src="..." class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+        `;
+        searchResult.appendChild(div);
+    });
 };
