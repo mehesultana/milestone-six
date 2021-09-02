5ef20f2a3f39c7d9fafa8a6c042bc7a0c178a2f9
mehesultana <sultanamehe01@gmail.com>
2021-09-03 01:27:54 +0600

    display temperature, city, dynamic weather icon

--- 36.5-bonsu-api-and-js-recap-concepts/temperature-hot/temperature.js
+++ 36.5-bonsu-api-and-js-recap-concepts/temperature-hot/temperature.js
@@ -2,7 +2,7 @@ const API_KEY = `5e01f3d0d64bf2360b2c6adfc383e0ad`;
 
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     //console.log(url);
 
     fetch(url)
@@ -10,6 +10,19 @@ const searchTemperature = () => {
         .then((data) => displayTemperature(data));
 };
 
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+};
+
 const displayTemperature = (temperature) => {
-    console.log(temperature);
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    //console.log(temperature);
+
+    //set weather icon
+    const url = `https://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
 };
